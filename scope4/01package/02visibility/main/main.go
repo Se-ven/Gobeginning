e933b3344c7a9e4bfcf218749948221c98db66bf
tuneboynick <tuneboy@tuneboynicks-Mac-Pro.local>
2017-02-27 10:56:09 -0700

    adding further typed or retyped practice.

---
+++ scope4/01package/02visibility/main/main.go
@@ -0,0 +1,11 @@
+package main
+
+import (
+	"fmt"
+	"github.com/Se-ven/Gobeginning/scope4/01package/02visibility/vis"
+)
+
+func main() {
+	fmt.Println(vis.MyName)
+	vis.PrintVar()
+}
---
+++ scope4/01package/02visibility/vis/name.go
@@ -0,0 +1,5 @@
+package vis
+
+//MyName is exported because it begins with a capital letter. Also note the package name....
+var MyName = "Nickolaus "
+var yourName = "-- Future rockstar programmer"
